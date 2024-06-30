35de1e7bdbaf0759d82edf94480de3188d47265d
AetheriumSlinky <horveli@hotmail.com>
2024-06-30 13:23:49 +0300

    final updates to logic and error handling

--- MTGCardBelcher.py
+++ MTGCardBelcher.py
@@ -4,82 +4,20 @@ import re
 import time
 import random
 
-with open("", "r") as oauth_file:
-    info = oauth_file.read().splitlines()
-
-UA = "MTGCardBelcher v1.0.0 by /u/MustaKotka (AetheriumSlinky)"
-
-r = praw.Reddit(
-    client_id=info[0],
-    client_secret=info[1],
-    password=info[2],
-    user_agent=UA,
-    username=info[3]
-)
+# MTGCardBelcher v1.0.0 by /u/MustaKotka (AetheriumSlinky)
 
 
-def comment_requires_action(comment_data: praw.Reddit.comment) -> bool:
-    """
-    Checks whether a comment has been replied to already or is excluded.
-    :param comment_data: Reddit's praw comment api data.
-    :return: True if comment requires action.
-    """
-    comment_double_bracket_matches = get_bracket_matches(comment_data.body)
-    submission_exclusions = [
-        # Weekly unjerk
-        re.search(r'.*unjerk.*thread.*', string=comment_data.body, flags=re.IGNORECASE)
-    ]
-
-    if comment_double_bracket_matches is None:
-        print("No double bracket matches (comment). " + comment_data.id)
-        return False
-    elif comment_data.author == 'MTGCardBelcher' or 'MTGCardFetcher':
-        print("Bot will not reply to itself or to the real CardFetcher (comment). " + comment_data.id)
-        return False
-    elif comment_data.submission.title in submission_exclusions:
-        print("Submission of the comment on exclusion list. " + comment_data.id)
-        return False
-    else:
-        print("Should reply to eligible comment: " + comment_data.id)
-        return True
-
-
-def submission_requires_action(submission_data: praw.Reddit.submission) -> bool:
-    """
-    Checks whether a submission has been replied to already or is excluded.
-    :param submission_data: Reddit's praw submission api data.
-    :return: True if submission requires action.
-    """
-    submission_double_bracket_match = get_bracket_matches(submission_data.selftext)
-    submission_exclusions = [
-        # Weekly unjerk
-        re.search(r'.*unjerk.*thread.*', string=submission_data.selftext, flags=re.IGNORECASE),
-        # Bottom scoring submissions
-        re.search(r'.*bottom.*scoring.*', string=submission_data.selftext, flags=re.IGNORECASE),
-    ]
-
-    if submission_double_bracket_match is None:
-        print("No double bracket matches (submission). " + submission_data.id)
-        return False
-    elif submission_data.author == 'MTGCardBelcher' or 'MTGCardFetcher':
-        print("Bot will not reply to itself or to the real CardFetcher (submission). " + submission_data.id)
-        return False
-    elif submission_data.title is submission_exclusions:
-        print("Submission on exclusion list. " + submission_data.id)
-        return False
-    else:
-        print("Should reply to eligible post: " + submission_data.id)
-        return True
-
-
-def get_bracket_matches(text: str) -> list:
+def get_regex_bracket_matches(text: str) -> list:
     """
     Regex searches for double square brackets (bot call) in text.
     :param text: Text to search.
     :return: A list of all matches.
     """
-    double_bracket_matches = re.findall(r'\[\[([^\[\]]*?)]]', text)
-    return double_bracket_matches
+    browser_double_bracket_matches = re.findall(r'''\\\[\\\[([^\\\[\]]+)\\]\\]''', text)
+    mobile_double_bracket_matches = re.findall(r'''\[\[([^\[\]]+)]]''', text)
+    all_matches = browser_double_bracket_matches + mobile_double_bracket_matches
+
+    return all_matches
 
 
 def get_image_links(reddit: praw.Reddit) -> list:
@@ -89,87 +27,185 @@ def get_image_links(reddit: praw.Reddit) -> list:
     :return: A list of image candidate links.
     """
     card_belcher = reddit.subreddit('MTGCardBelcher')
-    img_candidates = ['https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Ftwl36n1dil7d1.png']  # Pot of Greed
-    for image_submissions in card_belcher.hot(limit=50):
+    image_candidates = ['https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Ftwl36n1dil7d1.png']  # Pot of Greed
+    for image_submissions in card_belcher.new(limit=100):
         if "/r/MTGCardBelcher" not in image_submissions.url:
-            if re.search('(i.redd.it|i.imgur.com)' and image_submissions.link_flair_text == "Card Submission",
-                         image_submissions.url):
-                img_candidates.append(image_submissions.url)
-                print("Valid image submission: ", image_submissions.url)
-            else:
-                print("Invalid submission: ", image_submissions.url)
-    return img_candidates
+            if (re.search('(i.redd.it|i.imgur.com)', image_submissions.url)
+                    and image_submissions.link_flair_text == "Card Submission"):
+                image_candidates.append(image_submissions.url)
+    return image_candidates
 
 
-def bot_comment_action(
-        comment: praw.Reddit.comment,
-        image_links: list):
+def generate_reply_text(reply_text_string: str, image_link: str) -> str:
+    creature_type = random.choice([
+        "Horrors", "Kobolds", "Goblins", "Zombies", "Vampires", "Cephalids"
+    ])
+
+    bot_reply_text = "The " + creature_type + " have found the cards you're looking for:\n\n"
+
+    text_matches = get_regex_bracket_matches(reply_text_string)
+
+    for match_text_string in text_matches:
+        bot_reply_text += "[" + match_text_string + "](" + image_link + ")\n\n"
+
+    bot_reply_text += "*********\n\n^^^Submit ^^^your ^^^content ^^^at:\n\nr/MTGCardBelcher"
+
+    return bot_reply_text
+
+
+def comment_requires_action(comment_data: praw.Reddit.comment) -> bool:
+    """
+    Checks whether a comment has been replied to already or is excluded.
+    :param comment_data: Reddit's praw comment api data.
+    :return: True if comment requires action.
+    """
+    comment_double_bracket_matches: list = get_regex_bracket_matches(comment_data.body)
+    submission_exclusions = [
+        # Weekly unjerk
+        re.search(r'.*unjerk.*thread.*', string=comment_data.submission.title, flags=re.IGNORECASE)
+    ]
+
+    if comment_data.author.name == ('MTGCardBelcher' or 'MTGCardFetcher'):
+        print("Bot will not reply to itself or to the real CardFetcher (comment). " + comment_data.permalink)
+        return False
+    elif not comment_double_bracket_matches:
+        print("No matches in comment. " + comment_data.permalink)
+        return False
+    elif None not in submission_exclusions:
+        print("Parent submission of the comment on exclusion list. " + comment_data.permalink)
+        return False
+    else:
+        print(
+            "Should reply to eligible comment (" + str(comment_double_bracket_matches) + "): "
+            + comment_data.permalink
+        )
+        return True
+
+
+def submission_requires_action(submission_data: praw.Reddit.submission) -> bool:
+    """
+    Checks whether a submission has been replied to already or is excluded.
+    :param submission_data: Reddit's praw submission api data.
+    :return: True if submission requires action.
+    """
+    submission_double_bracket_matches = get_regex_bracket_matches(submission_data.selftext)
+    submission_exclusions = [
+        # Weekly unjerk
+        re.search(r'.*unjerk.*thread.*', string=submission_data.title, flags=re.IGNORECASE),
+        # Bottom scoring submissions
+        re.search(r'.*bottom.*scoring.*', string=submission_data.title, flags=re.IGNORECASE),
+    ]
+
+    if submission_data.author.name == ("MTGCardBelcher" or "MTGCardFetcher"):
+        print("Bot will not reply to itself or to the real CardFetcher (submission). " + submission_data.permalink)
+        return False
+    elif not submission_double_bracket_matches:
+        print("No matches in submission. " + submission_data.permalink)
+        return False
+    elif None not in submission_exclusions:
+        print("Submission on exclusion list. " + submission_data.permalink)
+        return False
+    else:
+        print(
+            "Should reply to eligible post (" + str(submission_double_bracket_matches) + "): "
+            + submission_data.permalink
+        )
+        return True
+
+
+def bot_comment_reply_action(
+        comment_data: praw.Reddit.comment,
+        image_submission_links: list):
     """
     Executes the reply action in an eligible comment.
-    :param comment: Comment to reply to.
-    :param image_links: A list of image candidate links.
+    :param comment_data: Comment to reply to.
+    :param image_submission_links: A list of image candidate links.
     """
-    creature_type = random.choice([
-        "Horrors", "Kobolds", "Goblins", "Zombies", "Vampires", "Cephalids"
-    ])
-    bot_reply_text = "The " + creature_type + " have found the cards you're looking for:\n\n"
-
-    text_matches = get_bracket_matches(comment.body)
-
-    for match_str in text_matches:
-        img_link = random.choice(image_links)
-        bot_reply_text += "[" + match_str + "](" + img_link + ")\n"
-    bot_reply_text += "\n*********\n\n^^^Submit ^^^your ^^^content ^^^at:\n\nr/MTGCardBelcher"
-
+    image_link = random.choice(image_submission_links)
+    reply_text = generate_reply_text(comment_data.body, image_link)
     try:
-        comment.reply(bot_reply_text)
-    except praw.exceptions.RedditAPIException:
-        print("Failed to reply to comment: " + comment.id)
+        comment.reply(reply_text)
+        print("Comment reply successful: " + comment_data.id)
+    except praw.exceptions.RedditAPIException as e:
+        print("Failed to reply to comment because Reddit has a problem: " + str(e))
 
 
-def bot_submission_action(
-        submission: praw.Reddit.submission,
-        image_links: list):
+def bot_submission_reply_action(
+        submission_data: praw.Reddit.submission,
+        image_submission_links: list):
     """
     Executes the reply action in an eligible submission.
-    :param submission: Submission to reply to.
-    :param image_links: A list of image candidate links.
+    :param submission_data: Submission to reply to.
+    :param image_submission_links: A list of image candidate links.
     """
-    creature_type = random.choice([
-        "Horrors", "Kobolds", "Goblins", "Zombies", "Vampires", "Cephalids"
-    ])
-    bot_reply_text = "The " + creature_type + " have found the cards you're looking for:\n\n"
-
-    text_matches = get_bracket_matches(submission.selftext)
-
-    for match_str in text_matches:
-        img_link = random.choice(image_links)
-        bot_reply_text += "[" + match_str + "](" + img_link + ")\n"
-    bot_reply_text += "\n*********\n\n^^^Submit ^^^your ^^^content ^^^at:\n\nr/MTGCardBelcher"
-
+    image_link = random.choice(image_submission_links)
+    reply_text = generate_reply_text(submission_data.selftext, image_link)
     try:
-        submission.reply(bot_reply_text)
-    except praw.exceptions.RedditAPIException:
-        print("Failed to reply to submission: " + submission.id)
+        submission.reply(reply_text)
+        print("Post reply successful: " + submission_data.id)
+    except praw.exceptions.RedditAPIException as e:
+        print("Failed to reply to post because Reddit has a problem: " + str(e))
 
 
-target = "magicthecirclejerking"
-img_links = get_image_links(r)
+if __name__ == "__main__":
+    print("Init...")
 
+    with open("oauth.txt", "r") as oauth_file:
+        info = oauth_file.read().splitlines()
 
-# Main loop
-while True:
-    image_refresh_timer = time.time()
+# Create a text file (here oauth.txt) with five rows:
+    # Client id
+    # Secret
+    # Account password
+    # User Agent information
+    # Account username
 
-    if time.time() - image_refresh_timer > 58:
-        img_links = get_image_links(r)
+    r = praw.Reddit(
+        client_id=info[0],
+        client_secret=info[1],
+        password=info[2],
+        user_agent=info[3],
+        username=info[4]
+    )
 
-    for mtcj_comment in r.subreddit(target).stream.comments(skip_existing=True):
-        if comment_requires_action(mtcj_comment):
-            bot_comment_action(mtcj_comment, img_links)
+    image_links = get_image_links(r)
+    image_refresh_timer = round(time.time())
+    target = "magicthecirclejerking"
+    mtcj_comments = r.subreddit(target).stream.comments(skip_existing=True, pause_after=2)
+    mtcj_submissions = r.subreddit(target).stream.submissions(skip_existing=True, pause_after=2)
 
-    for mtcj_submission in r.subreddit(target).stream.submissions(skip_existing=True):
-        if submission_requires_action(mtcj_submission):
-            bot_submission_action(mtcj_submission, img_links)
+    print("...init complete.")
 
-    time.sleep(10)
+    # Main loop
+    while __name__ == "__main__":
+
+        if round(time.time()) - image_refresh_timer > 59:
+            image_refresh_timer = round(time.time())
+            image_links = get_image_links(r)
+            print("Found " + str(len(image_links)) + " valid image submissions.")
+
+        for comment in mtcj_comments:
+            try:
+                if comment_requires_action(comment):
+                    bot_comment_reply_action(comment, image_links)
+                    print("Eligible comment reply finished.")
+            except AttributeError:  # No comments in stream results in None
+                print("Comment stream done.")
+                break
+            except praw.exceptions.RedditAPIException:
+                print("Reddit is experiencing problems (comments).")
+                break
+
+        for submission in mtcj_submissions:
+            try:
+                if submission_requires_action(submission):
+                    bot_submission_reply_action(submission, image_links)
+                    print("Eligible submission reply finished.")
+            except AttributeError:  # No submissions in stream results in None
+                print("Submission stream done.")
+                break
+            except praw.exceptions.RedditAPIException:
+                print("Reddit is experiencing problems (submissions).")
+                break
+
+        time.sleep(0.1)
--- UnitTest.py
+++ UnitTest.py
@@ -13,22 +13,22 @@ class UnitTest(unittest.TestCase):
 
     def test_match0(self):
         d = PrawMock('nothing matches')
-        v = MTGLardFetcher.get_bracket_matches(d.body)
+        v = MTGLardFetcher.get_regex_bracket_matches(d.body)
         self.assertEqual(v, [])
 
     def test_match1(self):
         d = PrawMock('[[x]]')
-        v = MTGLardFetcher.get_bracket_matches(d.body)
+        v = MTGLardFetcher.get_regex_bracket_matches(d.body)
         self.assertEqual(v[0], 'x')
 
     def test_match2(self):
         d = PrawMock('[[x]] some text [[y]]\n[[zzz]]')
-        v = MTGLardFetcher.get_bracket_matches(d.body)
+        v = MTGLardFetcher.get_regex_bracket_matches(d.body)
         self.assertEqual(v[1], 'y')
 
     def test_match3(self):
         d = PrawMock('[[x]] some text [[[[[[[[[y]]')
-        v = MTGLardFetcher.get_bracket_matches(d.body)
+        v = MTGLardFetcher.get_regex_bracket_matches(d.body)
         print(v)
         self.assertEqual(v[1], 'y')
 
