1b0d3932e5abd28f4d0cb14c28f7de2628ca9f70
AetheriumSlinky <horveli@hotmail.com>
2024-10-31 12:46:01 +0200

    fix logging file naming

--- functions/base_logger.py
+++ functions/base_logger.py
@@ -1,14 +1,18 @@
-"""Main logger config."""
+"""Main logger."""
 
 from datetime import datetime
 import logging
 from pathlib import Path
 
-root = Path(__file__).parent.parent
-
+loc = Path(__file__).parent.parent.joinpath('logs')
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-    filename=f"{root}\\logs\\{datetime.today().strftime('%Y_%m_%d')}.txt",
-    encoding='utf-8', level=logging.INFO,
-    format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M;%S'
-    )
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(filename=loc.joinpath("{:%Y_%m_%d}.txt".format(datetime.now())), encoding='utf-8')
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARNING)
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M;%S')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
