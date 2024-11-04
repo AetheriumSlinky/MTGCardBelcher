6cce35c24af9b6db3c7f6cb81c6d33e5d278ef16
AetheriumSlinky <horveli@hotmail.com>
2024-11-04 19:46:53 +0200

    timedrotatingfilehandler

--- functions/base_logger.py
+++ functions/base_logger.py
@@ -2,15 +2,19 @@
 
 from datetime import datetime
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 loc = Path(__file__).parent.parent.joinpath('logs')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-fh = logging.FileHandler(filename=loc.joinpath("{:%Y_%m_%d}.txt".format(datetime.now())), encoding='utf-8')
+fh = TimedRotatingFileHandler(filename=loc.joinpath("{:%Y_%m_%d}.txt".format(datetime.now())),
+                              when='midnight', encoding='utf-8')
 ch = logging.StreamHandler()
 ch.setLevel(logging.WARNING)
+
 formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M;%S')
+
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
