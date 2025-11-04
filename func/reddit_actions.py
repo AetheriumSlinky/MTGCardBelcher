6c75b4892a0ef80a1c8b559dc4e65f59d67e6211
AetheriumSlinky <horveli@hotmail.com>
2025-11-04 17:43:44 +0200

    accidental subm paste-in in the place of comm

--- func/reddit_actions.py
+++ func/reddit_actions.py
@@ -221,7 +221,7 @@ def submission_action(reddit_data: RedditData, target_subreddit, image_links: li
                 item_type = "submission"
                 submission_regex_matches = get_regex_bracket_matches(submission.selftext)
                 low_matches = [item.casefold() for item in submission_regex_matches]
-                if comment_requires_action(submission, submission_regex_matches):
+                if submission_requires_action(submission, submission_regex_matches):
                     collectible_match = reddit_data.collectibles.find_matching_collectible(low_matches)
                     if SpecialReplySettings.NFT_REPLIES_ON and collectible_match.timer.it_is_time():
                         special_reply(item_type, reddit_data, submission, collectible_match.name)
